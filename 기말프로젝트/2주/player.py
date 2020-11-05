47469944039dc1b7e2477f74d906a0869035408b
dmsdn16 <32299218+dmsdn16@users.noreply.github.com>
2020-11-05 21:31:02 +0900

    Update player.py
    플레이어 등장

--- 기말프로젝트/2주/player.py
+++ 기말프로젝트/2주/player.py
@@ -1,5 +1,33 @@
 from pico2d import *
-from gfw import *
+import gfw_image
+from gobj import *
+import helper
 
 class Player:
-    
+     KEY_MAP = {
+        (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
+        (SDL_KEYDOWN, SDLK_RIGHT): ( 1,  0),
+        (SDL_KEYDOWN, SDLK_DOWN):  ( 0, -1),
+        (SDL_KEYDOWN, SDLK_UP):    ( 0,  1),
+        (SDL_KEYUP, SDLK_LEFT):    ( 1,  0),
+        (SDL_KEYUP, SDLK_RIGHT):   (-1,  0),
+        (SDL_KEYUP, SDLK_DOWN):    ( 0,  1),
+        (SDL_KEYUP, SDLK_UP):      ( 0, -1),
+    }
+     KEYDOWN_SPACE = (SDL_KEYDOWN, SDLK_SPACE)
+     image = None
+
+     def __Init__(self):
+         self.pos = get_canvas_width()//2 ,get_canvas_height()//2
+
+         if image==None:
+            Player.image = gfw_image.load(RES_DIR + '/character.png')
+
+       
+     def draw(self):
+        Player.image = gfw_image.load(RES_DIR + '/character.png')
+        self.image.draw(550,500)
+
+
+   
+  
