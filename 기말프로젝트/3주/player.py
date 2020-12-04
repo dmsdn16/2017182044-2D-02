6f397dfa23eeffa8195db05ea21922a5cd43541f
dmsdn16 <32299218+dmsdn16@users.noreply.github.com>
2020-12-04 19:40:49 +0900

    벽 충돌

--- 기말프로젝트/3주/player.py
+++ 기말프로젝트/3주/player.py
@@ -6,6 +6,7 @@ import gfw
 
 
 
+
 class Player:
      KEY_MAP = {
         (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
@@ -21,7 +22,7 @@ class Player:
      image = None
 
      def __init__(self):
-       self.pos = 520,450
+       self.pos = 520,460
        self.action = 3
        self.fidx = 5
        self.delta = 0,0
@@ -39,36 +40,71 @@ class Player:
         self.image.clip_draw(sx,827,100,100, *self.pos,75,75)
 
      def update(self):
+       map1 = ((400,460),(340,460),(280,460),(220,400),\
+               (220,340),(160,280),(160,220),(160,160),\
+               (220,100),(280,100),(340,100),(400,100),(460,100),(520,100),(580,100),\
+               (580,460),(580,400),(520,340),(460,520),(520,520),\
+               (340,280),(400,280),(460,280),(520,280),(580,280),(580,220),(640,160))
        x,y = self.pos
        dx,dy = self.delta
        self.pos = x+dx, y+dy
        self.time += gfw.delta_time
        frame = self.time * 12
        self.fidx =int(frame) % 12
+
+       for i in range(27):
+            unx1 , uny1 = map1[i]
+            
+            if(self.action ==1 and x +60 == unx1 and y == uny1):# 우
+                self.pos =x,y
+            elif(self.action ==0 and x-60 == unx1 and y == uny1): #좌
+                self.pos =x,y
+            elif(self.action ==5 and x == unx1 and y + 60 == uny1): # 위
+                self.pos =x,y
+            elif(self.action ==4 and x == unx1 and y - 60 == uny1): # 아래
+                self.pos =x,y
+            print(x,y)
        
        if self.target is not None:
             ddx = -self.delta[0]
             helper.move_toward_obj(self)
             if self.target == None:
-                print("Removing target: ", self.targets[0], " from %d target(s)." % len(self.targets))
                 del self.targets[0]
                 if len(self.targets) > 0:
                     helper.set_target(self, self.targets[0])
+                    self.updateAction(self.delta[0],0)
                 else:
                     self.speed = 0
+                    self.updateAction(0, ddx)
      
      def get_bb(self):
          x,y = self.pos
-         return x - 50, y - 50 , x + 50, y + 50 
+         return x1 - 50, y1 - 50 , x1 + 50, y1 + 50 
 
    
      def updateDelta(self, ddx, ddy):
         dx,dy = self.delta
         dx += ddx * 60
         dy += ddy * 60
-        self.delta = dx, dy
+        if ddx != 0:
+            self.updateAction(dx, ddx)
+        if ddy !=0:
+            self.updateActionY(dy,ddy)
         self.delta = dx, dy
         
+     def updateAction(self, dx, ddx):
+        self.action = \
+            0 if dx < 0 else \
+            1 if dx > 0 else \
+            2 if ddx > 0 else 3
+        print(self.action)
+
+     def updateActionY(self,dy,ddy):
+        self.action = \
+            4 if dy < 0 else \
+            5 if dy >0 else \
+            6 if ddy > 0 else 7
+        print(self.action)
      
      def handle_event(self, e):
        
--- 기말프로젝트/3주/stone.py
+++ 기말프로젝트/3주/stone.py
@@ -8,8 +8,9 @@ class Stone:
     image = None
 
     def __init__(self):
-      self.pos1 = 350,150
+      self.pos1 = 340,150
       self.pos2 = 400,210
+      self.post = 460,520
       self.delta = 0,0
       self.target = None
       self.targets = []
@@ -19,4 +20,7 @@ class Stone:
     def draw(self):
         self.image.clip_draw(0, 0,100,100, *self.pos1,75,75)
         self.image.clip_draw(0, 0,100,100, *self.pos2,75,75)
+        self.image.clip_draw(0, 0,100,100, *self.post,75,75)
         
+
+   
